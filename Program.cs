023718b7f584d58f31c063ca44b84ed51e304195
Matvey Vizir <112975341+MaTTveIg@users.noreply.github.com>
2023-05-26 18:44:22 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using RandomLib;
+
+int randomNumber = RandomNumber.RandomiseNumber();
+Console.WriteLine($"Your random number is {randomNumber}");
+
+int randomNumber2 = RandomNumber.RandomiseInRange(5, 25);
+Console.WriteLine($"Your random number in range is {randomNumber2}");
+
+string randomString = RandomString.RandomiseString();
+Console.WriteLine($"Your random string is {randomString}");
+
+string randomString2 = RandomString.RandomiseOnlyText(10);
+Console.WriteLine($"Your random string of text is {randomString2}");
